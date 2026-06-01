3241026e6c36545f42b7681446f3fba2697e4b32
lukmanul6305 <lukmanulhakim6305@gmail.com>
2026-06-01 10:09:54 +0700

    feat: deploy database

--- backend/src/config/Database.js
+++ backend/src/config/Database.js
@@ -3,22 +3,15 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const db = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST || "localhost",
-    dialect: "mysql",
-    timezone: "+07:00", // WIB - Jakarta
-    logging: process.env.NODE_ENV === "development" ? console.log : false,
-    pool: {
-      max: 10,
-      min: 0,
-      acquire: 30000,
-      idle: 10000,
+const db = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "mysql",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
     },
-  }
-);
+  },
+  logging: console.log,
+});
 
 export default db;
